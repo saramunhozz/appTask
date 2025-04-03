f30baaf8326f32ae8e5453183c5526359a95afbc
Sara <saragabriely966@gmail.com>
2025-04-03 12:38:45 -0300

    aula0304

--- src/app/service/tasks.service.ts
+++ src/app/service/tasks.service.ts
@@ -1,23 +1,44 @@
 import { Injectable } from '@angular/core';
 
 import {Preferences } from '@capacitor/preferences'
+import { key } from 'ionicons/icons';
 
 export interface Task{
-  id: number,
-  title: string,
-  completed:boolean
+id: number,
+title: string,
+completed:boolean
 }
 
 @Injectable({
-  providedIn: 'root'
+providedIn: 'root'
 })
 export class TasksService {
 
 
+private TASK_KEY='tasks';
 
+//funcao
+async addTask(task:Task):Promise<void>{
 
-  constructor() { }
+const tasks = await this.getTask() || [];
+
+tasks.push(task)
+
+await Preferences.set({
+key: this.TASK_KEY ,
+value: JSON.stringify(task)
+})
+}
+
+// fucao busca task
+async getTask(): Promise<Task[ ]>{
+const {value} = await Preferences.get({key: this.TASK_KEY});
+return value ? JSON.parse(value) : []
+}
+
+constructor() { }
 
 
 
 }
+
